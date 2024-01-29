e4b669f8af097a57be23de242e0d595e51b90c30
Maurius Hill <146677371+mauriushill@users.noreply.github.com>
2024-01-29 22:48:06 +0000

    updates

--- app.rb
+++ app.rb
@@ -2,8 +2,10 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:home)
+end
+
+
+get("/holiday") do
+  erb(:holidayfinder)
 end
