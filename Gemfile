d2f095a034f013d212d86aba398f14944856f9e0
Maurius Hill <146677371+mauriushill@users.noreply.github.com>
2024-01-30 20:59:17 +0000

    updates

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source "https://rubygems.org"
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby "3.2.1"
+gem 'bootstrap', '~> 5.2.3'
 
 gem "sinatra"
 gem "sinatra-contrib"
--- app.rb
+++ app.rb
@@ -1,5 +1,7 @@
 require "sinatra"
 require "sinatra/reloader"
+require "http"
+require "json"
 
 get("/") do
   erb(:home)
@@ -7,5 +9,7 @@ end
 
 
 get("/holiday") do
+  raw_data = HTTP.get("https://calendarific.com/api/v2/holidays")
+  @api_data = JSON.parse(response.body)
   erb(:holidayfinder)
 end
